d584a52a7e89031772db4dc4869dcbfd74416e9b
Niachal Khanal <khanalnischal2075@gmail.com>
2024-01-06 01:11:38 +0545

    feat: frontend and backend integrated for auth

--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -2,12 +2,71 @@ import { Link } from "react-router-dom";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
 import AnymationWrapper from "../common/page-animation";
+import { useRef } from "react";
+import {Toaster, toast} from 'react-hot-toast';
+import axios from "axios";
 
 const UserAuthForm = ({ type }) => {
+
+
+  const userAuthThroughServer = (serverRoute,formData)=>{
+    axios.post('http://localhost:3000' + serverRoute, formData)
+
+    .then(({data})=>{
+      console.log(data)
+    })
+    .catch(({ response }) => {
+      toast.error(response.data.msg);
+  });
+  
+}
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    let serverRoute = type === "sign-in" ? "/signin" : "/signup";
+
+    // form data
+    let form = new FormData(formElement);
+    let formData = {};
+
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
+    }
+    console.log(formData);
+
+    let { fullname, email, password } = formData;
+    // Regex
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+    // form validation
+    if ( !email || !password) {
+
+      return toast.error("Please enter all fields");
+    }
+
+   if(fullname){
+    if (fullname.length < 3) {
+      return toast.error("Fullname must be at least 3 characters");
+    }
+   }
+    if (!emailRegex.test(email)) {
+      return toast.error("Please enter a valid email" );
+    }
+
+    if (!passwordRegex.test(password)) {
+      return toast.error("Password must be 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter",
+        );
+    }
+
+    userAuthThroughServer(serverRoute,formData)
+  };
+
   return (
     <AnymationWrapper keyValue={type}>
       <section className="h-cover flex items-center justify-center">
-        <form className="w-[80%] max-w-[400px]">
+        <Toaster/>
+        <form id="formElement" className="w-[80%] max-w-[400px]">
           <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
             {type === "sign-in" ? "Welcome back" : "Join us today"}
           </h1>
@@ -28,12 +87,16 @@ const UserAuthForm = ({ type }) => {
             icon="fi-rr-envelope"
           />
           <InputBox
-            name="passwoed"
+            name="password"
             type="password"
             placeholder="Password"
             icon="fi-rr-key"
           />
-          <button className="btn-dark center mt-14" type="submit">
+          <button
+            className="btn-dark center mt-14"
+            type="submit"
+            onClick={handlesubmit}
+          >
             {type.replace("-", " ")}
           </button>
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import 'dotenv/config'
 import bcrypt from 'bcrypt';
 import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
 
 // schema below
 import User from './Schema/User.js';
@@ -12,10 +14,27 @@ let PORT = 3000;
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 server.use(express.json());
-
+server.use(cors());
+server.options('*', cors());
 mongoose.connect(process.env.DB_LOCATION, {
     autoIndex: true
 });
+
+const formatDatatoSend = (user) => {
+    if (!process.env.SECRET_ACCESS_TOKEN_KEY) {
+        console.error('Missing SECRET_ACCESS_TOKEN_KEY in environment variables');
+        return null;
+    }
+
+    const accessToken = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_TOKEN_KEY, { expiresIn: '100h' });
+    return {
+        accessToken,
+        profile_img: user.personal_info.profile_img,
+        fullname: user.personal_info.fullname,
+        username: user.personal_info.username,
+    };
+};
+
 
 const generateUsername = async(email) => {
     let username = email.split("@")[0];
@@ -60,18 +79,44 @@ server.post("/signup", (req, res) => {
 
         user.save()
         .then((u) => {
-          return res.status(200).json({ user: u });
+          return res.status(200).json(formatDatatoSend(u));
         })
         .catch((err) => {
 
             if(err.code === 11000) {
-                return res.status(500).json({msg: "Email already exists"});
+                return res.status(403).json({msg: "Email already exists"});
             }
 
           return res.status(500).json({ 'Server error': err.message });
         });
     })
 
+})
+
+server.post("/signin",(req,res)=> {
+    let {email, password} = req.body;
+    User.findOne({"personal_info.email": email})
+    .then((user)=> {
+        if(!user) {
+            return res.status(403).json({msg: "User not found"})
+        }
+
+        bcrypt.compare(password, user.personal_info.password, (err, result) => {
+            if(err) {
+                return res.status(403).json({msg: "Error occured during authentication please try again"});
+            }
+
+            if(!result) {
+                return res.status(403).json({msg: "Incorrect password"});
+            }
+
+            return res.status(200).json(formatDatatoSend(user));
+        })
+    })
+    .catch(err => {
+        console.log(err.message);
+        return res.status(500).json({"Server error": err.message})
+    })
 })
 
 server.listen(PORT, () => {
