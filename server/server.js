3c90ae35c0b76694ad6fe30cdfd043350903185d
Niachal Khanal <khanalnischal2075@gmail.com>
2024-01-08 15:46:54 +0545

    added vercel frontend to backend

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for pass
 server.use(express.json());
 server.use(cors(
     {
-        origin: 'https://laganiforum.vercel.app',
+        origin: 'https://laganiforum-backend.vercel.app',
         credentials: true,
         methods: ['GET', 'POST', 'OPTIONS']
     }
